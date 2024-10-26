51b90a5a807776ee5129fd28a4b19e3b77e5328a
Nikita <modeniki10@gmail.com>
2024-10-26 15:32:28 +0300

    add login

--- main_site/time_to_eat/urls.py
+++ main_site/time_to_eat/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('users.urls')),
+    path('users/', include('users.urls')),
+    path("", include("app.urls"))
 ]
