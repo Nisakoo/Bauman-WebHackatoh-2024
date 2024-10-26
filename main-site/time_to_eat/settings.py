f73a3e6be92fe5cfeb14bc3ce6cc55ac49ca48a9
Luka <lucachobo@gmail.com>
2024-10-26 14:26:46 +0300

    add main-site/time_to_eat/settings.py

--- main-site/time_to_eat/settings.py
+++ main-site/time_to_eat/settings.py
@@ -22,6 +22,12 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Пользователи будут перенаправлены после входа в систему
+LOGIN_REDIRECT_URL = "home"
+
+# Пользователи будут перенаправлены после выхода в систему
+LOGOUT_REDIRECT_URL = "login"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
